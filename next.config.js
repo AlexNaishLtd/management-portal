d9850951b311053797c93fb848a91053b4750839
Sam Naish <sam.naish92@gmail.com>
2021-03-24 19:13:27 +0000

    next js boilerPlate

---
+++ next.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    // not sure what to put here?
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+
+function HomePage() {
+    return (
+        <div>
+            <h1>HomePage component</h1>
+            <span>Some placeholder text.</span>
+        </div>
+    )
+}
+  
+  export default HomePage
