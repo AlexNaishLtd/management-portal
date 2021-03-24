efe98e9fa3e60f7aa6c08f0f37d4798116d65a2b
Sam Naish <sam.naish92@gmail.com>
2021-03-24 19:35:34 +0000

    added jest to project

---
+++ src/test/index.test.js
@@ -0,0 +1,10 @@
+const { isTargetLikeServerless } = require('next/dist/next-server/server/config');
+const index = require('../pages/index');
+
+describe('index component renders', () => {
+
+    it('test for testing?', () => {
+        expect(10 + 11).toEqual(21);
+    })
+
+})
