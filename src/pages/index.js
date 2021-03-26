a805b9417809f3e8d2242c9cb47f5d9474520cda
samnaish <41487831+samnaish@users.noreply.github.com>
2021-03-26 19:01:58 +0000

    added tailwind and changed next config (#16)
    Co-authored-by: Sam Naish <sam.naish92@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import Link from 'next/link';
 
 function HomePage() {
     return (
         <div>
+            <Link href="/">
+               <a>Home</a>
+            </Link>
             <h1>HomePage component</h1>
             <span>Some placeholder text.</span>
         </div>
