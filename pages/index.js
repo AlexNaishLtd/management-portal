ebc7ff5da1f6f059f7a6ece374ff6dedd879849f
Alex Naish <alex@naish.io>
2021-03-26 22:03:39 +0000

    feat: add preview deployment action (#15)
    * feat: add preview deployment action
    
    * feat: remove next.config.js and let netlify handle it
    
    * fix: force latest netlify package
    
    * fix: explicitly state functions dir
    
    * feat: add status check
    
    * feat: update for prod deployments

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 
 function HomePage() {
-    return (
-        <div>
-            <Link href="/">
-               <a>Home</a>
-            </Link>
-            <h1>HomePage component</h1>
-            <span>Some placeholder text.</span>
-        </div>
-    )
+	return (
+		<div>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<h1>HomePage component</h1>
+				<span>Some placeholder text.</span>
+		</div>
+	)
 }
-  
-  export default HomePage
+
+export default HomePage;
