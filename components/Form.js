9183e25de80d60ee6f165ae7c9aa57a1e4ee8e0d
Sam Naish <sam.naish92@gmail.com>
2021-04-26 19:52:59 +0100

    fix: deleted curly brackets as they where clashing with pre-commit checks

--- components/Form.js
+++ components/Form.js
@@ -1,23 +1,34 @@
-import {useForm} from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
-import Select from "./Select";
-import TextArea from "./TextArea";
-import Input from "./Input";
-import CheckBox from "./CheckBox";
+import CheckBox from './CheckBox';
+import Input from './Input';
+import Select from './Select';
+import TextArea from './TextArea';
 
-const Form = ({}) => {
-    const {register, handleSubmit} = useForm();
+const Form = () => {
+    const { register, handleSubmit } = useForm();
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = (data) => console.log(data);
 
-    return(
+    return (
         <div className="flex items-center h-screen w-full">
             <div className="w-full bg-white rounded shadow-lg p-8 m-4">
                 <form className="mb-4" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                    <Input label="dummy input 1" name="first input" type="text" register={register} required/>
-                    <CheckBox label="dummy input 2" name="second input" register={register} required/>
+                    <Input
+                        label="dummy input 1"
+                        name="first input"
+                        type="text"
+                        register={register}
+                        required
+                    />
+                    <CheckBox
+                        label="dummy input 2"
+                        name="second input"
+                        register={register}
+                        required
+                    />
                     <div className="flex flex-col mb-4">
-                        <TextArea label="text area" name="txtname" register={register}/>
+                        <TextArea label="text area" name="txtname" register={register} />
                         <Select name="selectBox" register={register} required>
                             <option value="-">--</option>
                             <option value="dodo">dodo</option>
@@ -25,13 +36,13 @@ const Form = ({}) => {
                             <option value="something">something</option>
                         </Select>
                     </div>
-                    <input type="submit"/>
-                    <br/>
-                    <input type="reset"/>
+                    <input type="submit" />
+                    <br />
+                    <input type="reset" />
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Form;
