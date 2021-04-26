112ff91752550a55f8f7d7e340b4256a0a8aa4de
samnaish <41487831+samnaish@users.noreply.github.com>
2021-04-26 19:11:56 +0100

    feat: added github links to footer (#20)
    * feat: added github links to footer
    
    * fix: swapped names and changed name of component
    
    * fix: renamed Alink to FooterLink
    
    Co-authored-by: Sam Naish <sam.naish92@gmail.com>

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,29 @@
+import Copyright from './Copyright';
+import FooterLink from './FooterLink';
+
 const Footer = () => {
-    return(
-        <div className="h-10 bg-blue-500">
-            <span>A project developed by Alex and Sam Naish</span>
-        </div>
-    )
-}
+    return (
+        <footer className="text-center sm:mt-10 h-auto bg-blue-500">
+            <span className="m-8">A project developed by Sam and Alex Naish</span>
+            <div className="flex flex-wrap flex-row justify-center">
+                <div className="mx-8">
+                    <h4 className="pt-3">Community Links</h4>
+                    <div className="flex flex-row justify-center flex-col">
+                        <FooterLink href="https://github.com/alexnaish" label="Alex's Github" />
+                        <FooterLink href="https://github.com/samnaish" label="Sam's Github" />
+                    </div>
+                </div>
+                <div className="mx-8">
+                    <h4 className="pt-3">Terms</h4>
+                    <div className="flex flex-row justify-center flex-col">
+                        <FooterLink href="#" label="Terms of Service" />
+                        <FooterLink href="#" label="Terms of conditions" />
+                    </div>
+                </div>
+            </div>
+            <Copyright />
+        </footer>
+    );
+};
 
 export default Footer;
