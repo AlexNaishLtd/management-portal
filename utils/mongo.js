b30c5f81803266c52909973c31b43e400e96657a
Alex Naish <alex@naish.io>
2021-05-12 22:58:40 +0100

    fix: disconnect from mongo after awaiting next

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,8 +1,18 @@
 import mongoose from 'mongoose';
 
 export const connectionMiddleware = async (req, res, next) => {
+    const { context } = req.netlifyFunctionParams || {};
+
+    // Lambda functions automatically wait for any external connections to close
+    // As we're not manually disconnecting from mongodb
+    // because we want to reuse the connection (if possible!)
+    // We need to tell Netlify / AWS to respond as soon as we call res.send / res.json
+    // callbackWaitsForEmptyEventLoop -> false forces that behaviour
+    if (context) {
+        context.callbackWaitsForEmptyEventLoop = false;
+    }
+
     if (mongoose.connections[0].readyState) {
-        // Use current db connection
         return next();
     }
     try {
@@ -12,6 +22,7 @@ export const connectionMiddleware = async (req, res, next) => {
             useFindAndModify: false,
             useCreateIndex: true
         });
+
         await next();
     } catch (error) {
         console.log('Database error', error);
