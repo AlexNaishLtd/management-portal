0966a9263c4477272cd6e246c613ca08ccf9ce40
Sam Naish <sam.naish92@gmail.com>
2021-05-21 17:58:37 +0100

    created jsconfig file for easy component referancing

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import Nav from '../components/Nav';
-import Header from '../components/Header';
-import Content from '../components/Content';
-import Footer from '../components/Footer';
-import Form from '../components/Form';
+import Content from '@/components/Content';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Nav from '@/components/Nav';
 
 function HomePage() {
     return (
         <div className="flex flex-col h-screen justify-between">
-            <Nav/>
-            <Header text="Management Portal"/>
+            <Nav />
+            <Header text="Management Portal" />
             <Content>
-                <Form/>
                 <span>Some placeholder text.</span>
             </Content>
-            <Footer/>
+            <Footer />
         </div>
-    )
-};
- 
+    );
+}
+
 export default HomePage;
-  
--- pages/register.js
+++ pages/register.js
@@ -1,25 +1,27 @@
-import Nav from '../components/Nav';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Content from '../components/Content';
+import Content from '@/components/Content';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Nav from '@/components/Nav';
 
 const Register = () => {
     return (
         <div className="flex flex-col h-screen justify-between">
-            <Nav/>
-            <Header text="Register Page"/>
+            <Nav />
+            <Header text="Register Page" />
             <Content>
                 <div className="flex flex-col container mx-auto mx-4 my-6 border-solid border-4 border-light-blue-500">
-                    <input class="border focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mx-4 my-6"/>
-                    <input class="border focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mx-4 my-6"/>
+                    <input className="border focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mx-4 my-6" />
+                    <input className="border focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mx-4 my-6" />
                     <div className="">
-                        <button class="bg-red-500 hover:bg-red-700 rounded-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">Register</button>
+                        <button className="bg-red-500 hover:bg-red-700 rounded-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
+                            Register
+                        </button>
                     </div>
                 </div>
             </Content>
-            <Footer/>
+            <Footer />
         </div>
-    )
-}
+    );
+};
 
 export default Register;
