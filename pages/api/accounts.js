0db8b4c139aa2abf87747540ec212b65ca7f26ae
Alex Naish <alex@naish.io>
2021-06-26 18:44:48 +0100

    feat: add create account endpoint

--- pages/api/accounts.js
+++ pages/api/accounts.js
@@ -1,21 +1,37 @@
 import nc from 'next-connect';
 
-import AccountModel from '../../models/Account';
+import Account from '../../models/Account';
 import { connectionMiddleware } from '../../utils/mongo';
 
 const listAccountsHandler = async (req, res) => {
     try {
-        const accounts = await AccountModel.find({}, 'name contact_email', {
+        const accounts = await Account.find({}, 'name contact_email', {
             limit: 20
         });
 
         return res.status(200).json(accounts);
     } catch (error) {
-        console.log('======================');
-        console.log(error);
-        console.log('======================');
         return res.status(500).json({ success: false });
     }
 };
 
-export default nc().use(connectionMiddleware).get(listAccountsHandler);
+const createAccountHandler = async (req, res) => {
+    const newAccount = new Account({
+        name: req.body.name,
+        contact_email: req.body.contact_email,
+        contact_number: req.body.contact_number
+    });
+
+    try {
+        await newAccount.validate();
+    } catch (error) {
+        return res.status(400).json({
+            error
+        });
+    }
+
+    await newAccount.save();
+    return res.status(200).json(newAccount);
+};
+
+export default nc().use(connectionMiddleware).get(listAccountsHandler).post(createAccountHandler);
